846ff21014da3d301c869a2a52b98d9217b0c30e
WeiquanWu <weiquan.wu.13@gmail.com>
2018-09-20 15:32:57 +0800

    add find first non-repeating character

--- Arrays/ContainsDuplicate.cpp
+++ Arrays/ContainsDuplicate.cpp
@@ -33,3 +33,39 @@ public:
         return duplicate;
     }
 };
+
+/*Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+*/
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int lowest = -1;
+        unordered_map<char, int> charMap;
+        for(int i = 0; i<s.size(); i++)
+        {
+            //charMap[c] ++;
+            if(charMap.find(s[i]) != charMap.end())// already exists, mark as -1 since it's a duplicate
+            {
+                charMap[s[i]] = -1;
+            }
+            else
+            {
+                charMap[s[i]] = i;
+            }
+        }
+        
+        for(auto c : charMap)
+        {
+           if(c.second == -1 )
+           {continue;}
+            
+            if(c.second < lowest || lowest == -1)
+            {
+                lowest = c.second;
+            }
+        }
+      
+        return lowest;
+    }
+};
