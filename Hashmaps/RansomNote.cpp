7229b5a039982d798f46024b93c9b766b6c6cc3f
WeiquanWu <weiquan.wu.13@gmail.com>
2018-09-25 15:45:44 +0800

    new solutions, update queue using stacks

---
+++ Hashmaps/RansomNote.cpp
@@ -0,0 +1,144 @@
+
+/*
+Harold is a kidnapper who wrote a ransom note, but now he is worried it will be traced back to him through his handwriting. He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use only whole words available in the magazine. He cannot use substrings or concatenation to create the words he needs.
+
+Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+
+For example, the note is "Attack at dawn". The magazine contains only "attack at dawn". The magazine has all the right words, but there's a case mismatch. The answer is .
+
+Function Description
+
+Complete the checkMagazine function in the editor below. It must print  if the note can be formed using the magazine, or .
+
+checkMagazine has the following parameters:
+
+magazine: an array of strings, each a word in the magazine
+note: an array of strings, each a word in the ransom note
+Input Format
+
+The first line contains two space-separated integers,  and , the numbers of words in the  and the .. 
+The second line contains  space-separated strings, each . 
+The third line contains  space-separated strings, each .
+
+Constraints
+
+.
+Each word consists of English alphabetic letters (i.e.,  to  and  to ).
+Output Format
+
+Print Yes if he can use the magazine to create an untraceable replica of his ransom note. Otherwise, print No.
+
+Sample Input 0
+
+6 4
+give me one grand today night
+give one grand today
+Sample Output 0
+
+Yes
+Sample Input 1
+
+6 5
+two times three is not four
+two times two is four
+Sample Output 1
+
+No
+Explanation 1
+
+'two' only occurs once in the magazine.
+
+Sample Input 2
+
+7 4
+ive got a lovely bunch of coconuts
+ive got some coconuts
+Sample Output 2
+
+No
+Explanation 2
+
+Harold's magazine is missing the word .
+*/
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int HashFunction(int key)
+{
+   return key % 13; 
+}
+
+bool ransom_note(vector<string> magazine, vector<string> ransom) 
+{
+    bool isDup = true;
+    unordered_map <string, int> hashMap;
+    unordered_map <string, int> ransomMap;
+    for(unsigned i=0; i<magazine.size(); ++i)
+    {
+          hashMap[magazine[i]] += 1;
+    }
+   
+    for(unsigned i=0; i<ransom.size(); ++i)
+    {
+          ransomMap[ransom[i]] += 1;
+    }
+    
+    for(unsigned i=0; i<ransom.size(); ++i)
+    {
+        if (hashMap[ransom[i]]  != ransomMap[ransom[i]])
+        {
+            //cout << hashMap[ransom[i]]  + "vs" + ransomMap[ransom[i]];
+            return false;
+        }
+        if(hashMap.find(ransom[i]) == hashMap.end())
+        {
+            return false;
+        }
+        
+    }
+return isDup;
+}
+
+
+int main()
+{
+    int m;
+    int n;
+    cin >> m >> n;
+    vector<string> magazine(m);
+    for(int magazine_i = 0;magazine_i < m;magazine_i++){
+       cin >> magazine[magazine_i];
+    }
+    vector<string> ransom(n);
+    for(int ransom_i = 0;ransom_i < n;ransom_i++){
+       cin >> ransom[ransom_i];
+    }
+    if(ransom_note(magazine, ransom))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+    return 0;
+}
--- Stacks/QueueUsingStacks.cpp
+++ Stacks/QueueUsingStacks.cpp
@@ -1,39 +1,113 @@
-#include<stack>
+/*
+A queue is an abstract data type that maintains the order in which elements were added to it, allowing the oldest elements to be removed from the front and new elements to be added to the rear. This is called a First-In-First-Out (FIFO) data structure because the first element added to the queue (i.e., the one that has been waiting the longest) is always the first one to be removed.
 
+A basic queue has the following operations:
 
+Enqueue: add a new element to the end of the queue.
+Dequeue: remove the element from the front of the queue and return it.
+In this challenge, you must first implement a queue using two stacks. Then process  queries, where each query is one of the following  types:
+
+1 x: Enqueue element  into the end of the queue.
+2: Dequeue the element at the front of the queue.
+3: Print the element at the front of the queue.
+For example, a series of queries might be as follows:
+
+image
+
+Function Description
+
+Complete the put, pop, and peek methods in the editor below. They must perform the actions as described above.
+
+Input Format
+
+The first line contains a single integer, , the number of queries.
+
+Each of the next  lines contains a single query in the form described in the problem statement above. All queries start with an integer denoting the query , but only query  is followed by an additional space-separated value, , denoting the value to be enqueued.
+
+Constraints
+
+It is guaranteed that a valid answer always exists for each query of types  and .
+Output Format
+
+For each query of type , return the value of the element at the front of the fifo queue on a new line.
+
+Sample Input
+
+10
+1 42
+2
+1 14
+3
+1 28
+3
+1 60
+1 78
+2
+2
+Sample Output
+
+14
+14
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
 using namespace std;
-template <typename T>
-class MyQueue
-{
-public:
-	MyQueue();
-	void enqueue();
-	T peek();
-	T deequeue();
-private:
-	stack<T> stackNewestOnTop;
-	stack<T> oldestOnTop;
+
+class MyQueue {
+  
+    public:
+        stack<int> stack_newest_on_top, stack_oldest_on_top;   
+        void push(int x) {
+            stack_newest_on_top.push(x);
+        }
+
+        void pop() 
+        {
+          shiftStacks();    
+            stack_oldest_on_top.pop();
+        }
+
+        int front() 
+        {
+           shiftStacks();
+            return stack_oldest_on_top.top();
+        }
+    private:
+    void shiftStacks()
+    {
+        if(stack_oldest_on_top.empty())
+        {
+            while(!stack_newest_on_top.empty())
+            {
+                stack_oldest_on_top.push(stack_newest_on_top.top());
+                stack_newest_on_top.pop();
+            }
+        }
+    }
 };
-template <typename T>
-void MyQueue::enqueue()
-{
 
-}
-
-template <typename T> 
-T MyQueue::deequeue()
-{
-
-}
-template <typename T> 
-T MyQueue::peek()
-{
-	
-}
-
-int main(int argc, char const *argv[])
-{
-
-	/* code */
-	return 0;
+int main() {
+    MyQueue q1;
+    int q, type, x;
+    cin >> q;
+    
+    for(int i = 0; i < q; i++) {
+        cin >> type;
+        if(type == 1) {
+            cin >> x;
+            q1.push(x);
+        }
+        else if(type == 2) {
+            q1.pop();
+        }
+        else cout << q1.front() << endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
 }
